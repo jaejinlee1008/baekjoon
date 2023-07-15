7fbd7354442e322785d4c2644c17222bfb6b3f84
leejaejin <ghkfuddks@naver.com>
2023-07-15 23:56:34 +0900

    23/07/15_1문제

---
+++ solved.ac/src/solved/ac/좌표_정렬하기_2.java
@@ -0,0 +1,22 @@
+package solved.ac;
+
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.util.ArrayList;
+import java.util.List;
+
+public class 좌표_정렬하기_2 {
+	public static void main(String[] args) throws NumberFormatException, IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+		
+		long N = Long.parseLong(br.readLine());
+		List<String> list = new ArrayList<>();
+		for(long i=0;i<N;i++) {
+			
+		}
+	}
+}
