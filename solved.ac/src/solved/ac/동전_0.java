fbc6dae0c7af297127a512eda5df61f725af2d50
leejaejin <ghkfuddks@naver.com>
2023-07-22 20:34:06 +0900

    23/07/22_1문제

---
+++ solved.ac/src/solved/ac/동전_0.java
@@ -0,0 +1,27 @@
+package solved.ac;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class µ¿Àü_0 {
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st = new StringTokenizer(br.readLine());
+		int N = Integer.parseInt(st.nextToken());
+		int K = Integer.parseInt(st.nextToken());
+		int[] arr = new int[N];
+		int count=0;
+		for(int i=0;i<N;i++) {
+			arr[i] = Integer.parseInt(br.readLine());
+		}
+		for(int i=N-1;i>=0;i--) {
+			if(arr[i]<=K) {
+				count+=K/arr[i];
+				K%=arr[i];
+			}
+		}
+		System.out.println(count);
+	}
+}
