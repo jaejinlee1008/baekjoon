2a0da9adff2d7aceea6cba8f02916218da81cfed
jaejin <ghkfuddks@naver.com>
2023-08-10 16:36:32 +0900

    23/08/10_*분할정복*

---
+++ baekjoon_UTF8/src/study/곱셈.java
@@ -0,0 +1,31 @@
+package study;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class 곱셈 {
+	static int A;
+	static int B;
+	static int C;
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st = new StringTokenizer(br.readLine());
+		A = Integer.parseInt(st.nextToken());
+		B = Integer.parseInt(st.nextToken());
+		C = Integer.parseInt(st.nextToken());
+		System.out.println(pow(A,B));
+	}
+	private static long pow(long A,long B) {
+		if(B==1) {
+			return A%C;
+		}
+		long num = pow(A,B/2);
+		if(B%2==0) {
+			return num*num%C;
+		}else {
+			return (num*num%C)*A%C;
+		}
+	}
+}
