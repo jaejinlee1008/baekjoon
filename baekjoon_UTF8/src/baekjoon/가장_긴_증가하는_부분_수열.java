787106b909aa8ce223e425f75d54dff0ce9d5729
jaejin <ghkfuddks@naver.com>
2023-08-15 16:29:45 +0900

    23/08/15_LIS_DP

--- baekjoon_UTF8/src/baekjoon/가장_긴_증가하는_부분_수열.java
+++ baekjoon_UTF8/src/baekjoon/가장_긴_증가하는_부분_수열.java
@@ -3,14 +3,13 @@ package baekjoon;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.HashSet;
-import java.util.Set;
 import java.util.StringTokenizer;
 
 public class 가장_긴_증가하는_부분_수열 {
 	static int N;
 	static int[] input;
 	static int[] dp;
+	static int answer = Integer.MIN_VALUE;
 	public static void main(String[] args) throws NumberFormatException, IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		int N = Integer.parseInt(br.readLine());
@@ -28,10 +27,16 @@ public class 가장_긴_증가하는_부분_수열 {
 			return;
 		}
 		for(int i=2;i<=N;i++) {
-			for(int j=3;j<=N;j++) {
-				
+			int max=0;
+			for(int j=1;j<i;j++) {
+				if(input[j]<input[i]) {
+					max = Math.max(max,dp[j]);
+				}
 			}
+			dp[i]=max+1;
+			answer = Math.max(answer, dp[i]);
 		}
+		System.out.println(answer);
 	}
 
 }
