67e0043031c634ca0800e759cbb41cbebf9b1095
jaejin <ghkfuddks@naver.com>
2023-08-24 15:38:32 +0900

    23/08/24_분할정복,피보나치

---
+++ baekjoon_UTF8/src/baekjoon/피보나치_수6.java
@@ -0,0 +1,38 @@
+package baekjoon;
+
+import java.util.Scanner;
+
+public class 피보나치_수6 {
+	static long MOD = 1000000007;
+	static long[][] matrix = {{1,1},{1,0}};
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		long N = sc.nextLong();
+		
+		long[][] A = {{1,1},{1,0}};
+		
+		System.out.println(pow(A,N-1)[0][0]);
+	}
+	private static long[][] pow(long[][] A,long n){
+		if(n<=1) {
+			return A;
+		}
+		
+		long[][] tmp = pow(A,n/2);
+		
+		tmp = mul(tmp,tmp);
+		if(n%2!=0) {
+			tmp =  mul(tmp,matrix);
+		}
+		return tmp;
+	}
+	private static long[][] mul(long[][] o1, long[][] o2) {
+		
+		long[][] tmp = new long[2][2];
+		tmp[0][0] = ((o1[0][0]*o2[0][0])+(o1[0][1]*o2[1][0]))%MOD;
+		tmp[0][1] = ((o1[0][0]*o2[0][1])+(o1[0][1]*o2[1][1]))%MOD;
+		tmp[1][0] = ((o1[1][0]*o2[0][0])+(o1[1][1]*o2[1][0]))%MOD;
+		tmp[1][1] = ((o1[1][0]*o2[0][1])+(o1[1][1]*o2[1][1]))%MOD;
+		return tmp;
+	}
+}
