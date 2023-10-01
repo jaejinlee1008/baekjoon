34e01a2b5b38979424aac5168b3863e2da64e153
jaejin <ghkfuddks@naver.com>
2023-10-01 10:33:07 +0900

    23/10/01_별찍기

---
+++ baekjoon_UTF8_2/src/baekjoon/별_찍기_3.java
@@ -0,0 +1,20 @@
+package baekjoon;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class 별_찍기_3 {
+	public static void main(String[] args) throws NumberFormatException, IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		int N = Integer.parseInt(br.readLine());
+		
+		for(int i = 0;i<N;i++) {
+			for(int j=i;j<N;j++) {
+				System.out.print("*");
+			}
+			System.out.println();
+		}
+	}
+}
+
