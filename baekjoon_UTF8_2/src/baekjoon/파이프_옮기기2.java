e902eb14103c2b74df9c2c6074e00a27bf22906d
jaejin <ghkfuddks@naver.com>
2023-10-13 00:35:31 +0900

    23/10/13_3차원DP

--- baekjoon_UTF8_2/src/baekjoon/파이프_옮기기2.java
+++ baekjoon_UTF8_2/src/baekjoon/파이프_옮기기2.java
@@ -7,42 +7,39 @@ import java.util.StringTokenizer;
 
 public class 파이프_옮기기2 {
 	static int N;
-	static int[][][] arr;
-	static boolean[][] visited;
+	static long[][][] dp;
+	static int[][] map;
 	public static void main(String[] args) throws NumberFormatException, IOException {
 		BufferedReader br= new BufferedReader(new InputStreamReader(System.in));
 		N = Integer.parseInt(br.readLine());
-		arr = new int[N+1][N+1][3];// 가로,세로,대각선
-		visited = new boolean[N+1][N+1];
+		dp = new long[N+1][N+1][3];// 가로,세로,대각선
+		map = new int[N+1][N+1];
 		for(int i=1;i<=N;i++) {
 			StringTokenizer st = new StringTokenizer(br.readLine());
 			for(int j=1;j<=N;j++) {
 				int num = Integer.parseInt(st.nextToken());
-				if(num==1) visited[i][j]=true;
-				for(int k=0;k<3;k++) {
-					arr[i][j][k]=num;
-				}
+				map[i][j]=num;
 			}
 		}
-		arr[1][1][0]=1;
-		arr[1][2][0]=1;
-		arr[1][1][1]=1;
-		arr[1][2][1]=1;
-		arr[1][1][2]=1;
-		arr[1][2][2]=1;
+		if(map[N][N]==1) {
+			System.out.println(0);
+			return;
+		}
+		
+		dp[1][2][0]=1;
 		
 		for(int i=1;i<=N;i++) {
-			for(int j=2;j<=N;j++) {
-				if(arr[i][j][0])
-				arr[i][j][0] = arr[i][j-1][0]+arr[i][j-1][2];
-				arr[i][j][1] = arr[i-1][j][1]+arr[i-1][j][2];
-				arr[i][j][2] = arr[i-1][j-1][0]+arr[i-1][j-1][1]+arr[i-1][j-1][2];
+			for(int j=3;j<=N;j++) {
+				if(map[i][j]==1) continue;
+				dp[i][j][0] = dp[i][j-1][0]+dp[i][j-1][2];
+				if(i==1) continue;
+				dp[i][j][1] = dp[i-1][j][1]+dp[i-1][j][2];
+				if(map[i-1][j]==1||map[i][j-1]==1) continue;
+				dp[i][j][2] = dp[i-1][j-1][0]+dp[i-1][j-1][1]+dp[i-1][j-1][2];
 			}
 		}
-		System.out.println(arr[N][N][0]+arr[N][N][1]+arr[N][N][2]);
+		System.out.println(dp[N][N][0]+dp[N][N][1]+dp[N][N][2]);
 	}
 	
-	private static boolean check() {
-		
-	}
+	
 }
