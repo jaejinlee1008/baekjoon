413ca36b1e64b150625a9e1a978aa12479d5bd4c
jaejin <ghkfuddks@naver.com>
2023-11-10 23:53:57 +0900

    23/11/10_브루트포스

---
+++ baekjoon_UTF8_2/src/baekjoon/약수_구하기.java
@@ -0,0 +1,27 @@
+package baekjoon;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class 약수_구하기 {
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st = new StringTokenizer(br.readLine());
+		int N = Integer.parseInt(st.nextToken());
+		int K = Integer.parseInt(st.nextToken());
+		int cnt=0;
+		int result=0;
+		for(int i=1;i<=N;i++) {
+			if(N%i==0) {
+				cnt++;
+			}
+			if(cnt==K){
+				result = i;
+				break;
+			}
+		}
+		System.out.println(result);
+	}
+}
