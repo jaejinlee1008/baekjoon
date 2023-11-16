234b1c57e0f6eab1cb4bad535d49b1ba27298c4d
jaejin <ghkfuddks@naver.com>
2023-11-16 20:08:00 +0900

    23/11/16_date

---
+++ baekjoon_UTF8_2/src/baekjoon/오늘_날짜.java
@@ -0,0 +1,11 @@
+package baekjoon;
+
+import java.time.LocalDate;
+
+public class 오늘_날짜 {
+	public static void main(String[] args) {
+		LocalDate now = LocalDate.now();
+        
+        System.out.println(now);
+	}
+}
