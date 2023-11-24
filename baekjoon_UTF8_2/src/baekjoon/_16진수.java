399e87575b136a9054a6c77990af766eccd6cf78
jaejin <ghkfuddks@naver.com>
2023-11-24 21:49:35 +0900

    23/11/24_수학

--- baekjoon_UTF8_2/src/baekjoon/_16진수.java
+++ baekjoon_UTF8_2/src/baekjoon/_16진수.java
@@ -9,6 +9,7 @@ public class _16진수 {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		String str = br.readLine();
 		int result = Integer.parseInt(str, 16);
+		
 		System.out.println(result);
 	}
 }
