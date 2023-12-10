a36bfed018b285204f31871d8d25fbd2fa099862
jaejin <ghkfuddks@naver.com>
2023-12-10 23:52:33 +0900

    23/12/10_구현중

---
+++ baekjoon_UTF8_2/src/baekjoon/사이클_게임.java
@@ -0,0 +1,17 @@
+package baekjoon;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class 사이클_게임 {
+	static int n,m;
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st = new StringTokenizer(br.readLine());
+		n = Integer.parseInt(st.nextToken());
+		m = Integer.parseInt(st.nextToken());
+		
+	}
+}
