ecbee40fe228f33bb2cd1ee4170ca7feaaba2ca8
jaejin <ghkfuddks@naver.com>
2023-12-15 09:24:23 +0900

    23/12/15_크루스칼

--- baekjoon_UTF8_2/src/baekjoon/사이클_게임.java
+++ baekjoon_UTF8_2/src/baekjoon/사이클_게임.java
@@ -7,16 +7,44 @@ import java.util.StringTokenizer;
 
 public class 사이클_게임 {
 	static int n,m;
+	static int[] parents;
+	static void make() {
+		parents = new int[n];
+		for(int i=0;i<n;i++) {
+			parents[i]=i;
+		}
+	}
+	static int find(int a) {
+		if(parents[a]==a) return a;
+		return parents[a] = find(parents[a]);
+	}
+	
+	static boolean union(int a, int b) {
+		int aroot = find(a);
+		int broot = find(b);
+		if(aroot==broot) return false;
+		else {
+			parents[broot] = aroot;
+			return true;
+		}
+	}
 	public static void main(String[] args) throws IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		StringTokenizer st = new StringTokenizer(br.readLine());
 		n = Integer.parseInt(st.nextToken());
 		m = Integer.parseInt(st.nextToken());
-		for(int i=0;i<m;i++) {
+		make();
+		int result=0;
+		boolean flag=false;
+		for(int i=1;i<=m;i++) {
 			st = new StringTokenizer(br.readLine());
 			int a = Integer.parseInt(st.nextToken());
 			int b = Integer.parseInt(st.nextToken());
-			
+			if(!union(a,b)&&!flag) {
+				result=i;
+				flag=true;
+			}
 		}
+		System.out.println(result);
 	}
 }
