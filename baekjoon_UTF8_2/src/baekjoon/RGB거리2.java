82be0f5d119c7daf4580ad538893520813bd8a37
jaejin <ghkfuddks@naver.com>
2023-12-19 12:15:41 +0900

    23/12/19_구현중...

---
+++ baekjoon_UTF8_2/src/baekjoon/RGB거리2.java
@@ -0,0 +1,10 @@
+package baekjoon;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+
+public class RGB거리2 {
+	public static void main(String[] args) {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+	}
+}
