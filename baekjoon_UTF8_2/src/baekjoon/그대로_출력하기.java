92fa0481cfaf5e96631c8a790af413114c98fedf
jaejin <ghkfuddks@naver.com>
2023-12-23 21:49:19 +0900

    23/12/23_문자열

---
+++ baekjoon_UTF8_2/src/baekjoon/그대로_출력하기.java
@@ -0,0 +1,15 @@
+package baekjoon;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class 그대로_출력하기 {
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		String str="";
+		while((str = br.readLine()) != null){
+			System.out.println(str);
+		}
+	}
+}
