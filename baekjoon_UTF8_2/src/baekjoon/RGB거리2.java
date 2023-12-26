f47a0df0d1bf2e63eb7068b7fa6c0a8a1abf5555
jaejin <ghkfuddks@naver.com>
2023-12-26 17:25:33 +0900

    23/12/26_2차원DP

--- baekjoon_UTF8_2/src/baekjoon/RGB거리2.java
+++ baekjoon_UTF8_2/src/baekjoon/RGB거리2.java
@@ -6,9 +6,19 @@ import java.io.InputStreamReader;
 import java.util.StringTokenizer;
 
 public class RGB거리2 {
+	static int N;
+	static int[][] input;
 	public static void main(String[] args) throws IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		StringTokenizer st = new StringTokenizer(br.readLine());
+		N = Integer.parseInt(br.readLine());
+		input = new int[N+1][3];
+		for(int i=1;i<=N;i++) {
+			StringTokenizer st = new StringTokenizer(br.readLine());
+			input[i][0] = Integer.parseInt(st.nextToken());
+			input[i][1] = Integer.parseInt(st.nextToken());
+			input[i][2] = Integer.parseInt(st.nextToken());
+		}
+		
 	
 	}
 }
