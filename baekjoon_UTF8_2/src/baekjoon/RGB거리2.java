f00188c09d8b48851695216695d377817e3b9223
jaejin <ghkfuddks@naver.com>
2024-01-05 15:46:52 +0900

    24/01/05_DP, 첫번째 집 고정 방식으로 변경, 통과

--- baekjoon_UTF8_2/src/baekjoon/RGB거리2.java
+++ baekjoon_UTF8_2/src/baekjoon/RGB거리2.java
@@ -8,133 +8,39 @@ import java.util.StringTokenizer;
 
 public class RGB거리2 {
 	static int N;
-	static long[][] input;
+	static int[][] input;
+	static int[][] dp;
+	static int INF = 1000*1000;
 	public static void main(String[] args) throws IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		N = Integer.parseInt(br.readLine());
-		input = new long[N+1][3];
+		input = new int[N+1][3];
+		dp = new int[N+1][3];
 		for(int i=1;i<=N;i++) {
 			StringTokenizer st = new StringTokenizer(br.readLine());
 			input[i][0] = Integer.parseInt(st.nextToken());
 			input[i][1] = Integer.parseInt(st.nextToken());
 			input[i][2] = Integer.parseInt(st.nextToken());
 		}
-		long[][][][] dp = new long[N+1][3][3][2];
-		long result = Long.MAX_VALUE;
-		for(int i=1;i<=N;i++) {
-			if(i==N) {
-				if(dp[i-1][1][2][1]!=0&&dp[i-1][1][0][1]!=0) {
-					result = Math.min(result,input[i][0]+Math.min(dp[i-1][1][0][0],dp[i-1][1][2][0]));
-				}else if(dp[i-1][1][2][1]!=0) {
-					result =  Math.min(result, input[i][0]+dp[i-1][1][2][0]);
-				}else if(dp[i-1][1][0][1]!=0) {
-					result = Math.min(result, input[i][0]+dp[i-1][1][0][0]);
+		int result = INF;
+		for(int k=0;k<3;k++) {
+			for(int i=0;i<3;i++) {
+				if(i==k) dp[1][i] = input[1][i];
+				else {
+					dp[1][i] = INF;
 				}
-				
-				if(dp[i-1][2][1][1]!=0&&dp[i-1][2][0][1]!=0) {
-					result = Math.min(result, input[i][0]+Math.min(dp[i-1][2][0][0],dp[i-1][2][1][0]));
-				}else if(dp[i-1][2][1][1]!=0) {
-					result = Math.min(result, input[i][0]+dp[i-1][2][1][0]);
-				}else if(dp[i-1][2][0][1]!=0) {
-					result = Math.min(result, input[i][0]+dp[i-1][2][0][0]);
-				}
-				
-				if(dp[i-1][0][2][1]!=1&&dp[i-1][0][1][1]!=1) {
-					result = Math.min(result, input[i][1]+Math.min(dp[i-1][0][1][0],dp[i-1][0][2][0]));
-				}else if(dp[i-1][0][2][1]!=1) {
-					result = Math.min(result, input[i][1]+dp[i-1][0][2][0]);
-				}else if(dp[i-1][0][1][1]!=1) {
-					result = Math.min(result, input[i][1]+dp[i-1][0][1][0]);
-				}
-				
-				if(dp[i-1][2][1][1]!=1&&dp[i-1][2][0][1]!=1) {
-					result = Math.min(result, input[i][1]+Math.min(dp[i-1][2][0][0],dp[i-1][2][1][0]));
-				}else if(dp[i-1][2][1][1]!=1) {
-					result = Math.min(result, input[i][1]+dp[i-1][2][1][0]);
-				}else if(dp[i-1][2][0][1]!=1) {
-					result = Math.min(result, input[i][1]+dp[i-1][2][0][0]);
-				}
-				
-				if(dp[i-1][0][2][1]!=2&&dp[i-1][0][1][1]!=2) {
-					result = Math.min(result, input[i][2]+Math.min(dp[i-1][0][1][0],dp[i-1][0][2][0]));
-				}else if(dp[i-1][0][2][1]!=2) {
-					result = Math.min(result, input[i][2]+dp[i-1][0][2][0]);
-				}else if(dp[i-1][0][1][1]!=2) {
-					result = Math.min(result, input[i][2]+dp[i-1][0][1][0]);
-				}
-				if(dp[i-1][1][2][1]!=2&&dp[i-1][1][0][1]!=2) {
-					result = Math.min(result, input[i][2]+Math.min(dp[i-1][1][0][0],dp[i-1][1][2][0]));
-				}else if(dp[i-1][1][2][1]!=2) {
-					result = Math.min(result, input[i][2]+dp[i-1][1][2][0]);
-				}else if(dp[i-1][1][0][1]!=2) {
-					result = Math.min(result, input[i][2]+dp[i-1][1][0][0]);
-				}
-			}else if(i==2) {
-				dp[i][0][1][0] = input[i][0]+Math.min(dp[i-1][1][0][0],dp[i-1][1][2][0]);
-				dp[i][0][1][1] = 1;
-				dp[i][0][2][0] = input[i][0]+Math.min(dp[i-1][2][0][0],dp[i-1][2][1][0]);
-				dp[i][0][2][1] = 2;
-				
-				dp[i][1][0][0] = input[i][1]+Math.min(dp[i-1][0][1][0],dp[i-1][0][2][0]);
-				dp[i][1][0][1] = 0;
-				dp[i][1][2][0] = input[i][1]+Math.min(dp[i-1][2][0][0],dp[i-1][2][1][0]);
-				dp[i][1][2][1] = 2;
-				
-				dp[i][2][0][0] = input[i][2]+Math.min(dp[i-1][0][1][0],dp[i-1][0][2][0]);
-				dp[i][2][0][1] = 0;
-				dp[i][2][1][0] = input[i][2]+Math.min(dp[i-1][1][0][0],dp[i-1][1][2][0]);
-				dp[i][2][1][1] = 1;
-			}else {
-				if(dp[i-1][1][0][0]>=dp[i-1][1][2][0]) {
-					dp[i][0][1][0] = input[i][0]+dp[i-1][1][2][0];
-					dp[i][0][1][1] = dp[i-1][1][2][1];
-				}else {
-					dp[i][0][1][0] = input[i][0]+dp[i-1][1][0][0];
-					dp[i][0][1][1] = dp[i-1][1][0][1];
-				}
-				if(dp[i-1][2][0][0]>=dp[i-1][2][1][0]) {
-					dp[i][0][2][0] = input[i][0]+dp[i-1][2][1][0];
-					dp[i][0][2][1] = dp[i-1][2][1][1];
-				}else {
-					dp[i][0][2][0] = input[i][0]+dp[i-1][2][0][0];
-					dp[i][0][2][1] = dp[i-1][2][0][1];
-				}
-				
-				if(dp[i-1][0][1][0]>=dp[i-1][0][2][0]) {
-					dp[i][1][0][0] = input[i][1]+dp[i-1][0][2][0];
-					dp[i][1][0][1] = dp[i-1][0][2][1];
-				}else {
-					dp[i][1][0][0] = input[i][1]+dp[i-1][0][1][0];
-					dp[i][1][0][1] = dp[i-1][0][1][1];
-				}
-				
-				if(dp[i-1][2][0][0]>=dp[i-1][2][1][0]) {
-					dp[i][1][2][0] = input[i][1]+dp[i-1][2][1][0];
-					dp[i][1][2][1] = dp[i-1][2][1][1];
-				}else {
-					dp[i][1][2][0] = input[i][1]+dp[i-1][2][0][0];
-					dp[i][1][2][1] = dp[i-1][2][0][1];
-				}
-				
-				if(dp[i-1][0][1][0]>=dp[i-1][0][2][0]) {
-					dp[i][2][0][0] = input[i][2]+dp[i-1][0][2][0];
-					dp[i][2][0][1] = dp[i-1][0][2][1];
-				}else {
-					dp[i][2][0][0] = input[i][2]+dp[i-1][0][1][0];
-					dp[i][2][0][1] = dp[i-1][0][1][1];
-				}
-				
-				if(dp[i-1][1][0][0]>=dp[i-1][1][2][0]) {
-					dp[i][2][1][0] = input[i][2]+dp[i-1][1][2][0];
-					dp[i][2][1][1] = dp[i-1][1][2][1];	
-				}else {
-					dp[i][2][1][0] = input[i][2]+dp[i-1][1][0][0];
-					dp[i][2][1][1] = dp[i-1][1][0][1];
-				}
-				
+			}
+			
+			for(int i=2;i<=N;i++) {
+				dp[i][0] = Math.min(dp[i-1][1], dp[i-1][2])+input[i][0];
+				dp[i][1] = Math.min(dp[i-1][0], dp[i-1][2])+input[i][1];
+				dp[i][2] = Math.min(dp[i-1][0], dp[i-1][1])+input[i][2];
+			}
+			
+			for(int i=0;i<3;i++) {
+				if(i!=k) result = Math.min(result, dp[N][i]);
 			}
 		}
 		System.out.println(result);
-		
 	}
 }
