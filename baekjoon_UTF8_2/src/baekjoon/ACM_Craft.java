3cde960885d3e0e83d94a3a447bf26a4142ab976
jaejin <ghkfuddks@naver.com>
2024-01-08 22:33:51 +0900

    24/01/08_구현중...

--- baekjoon_UTF8_2/src/baekjoon/ACM_Craft.java
+++ baekjoon_UTF8_2/src/baekjoon/ACM_Craft.java
@@ -1,7 +1,10 @@
 package baekjoon;
 
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+
 public class ACM_Craft {
 	public static void main(String[] args) {
-		
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 	}
 }
