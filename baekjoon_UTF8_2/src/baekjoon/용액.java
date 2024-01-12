68c0d4ba24cd039991e26eb74b7c42bb7e37b64a
jaejin <ghkfuddks@naver.com>
2024-01-12 22:19:05 +0900

    24/01/02_투포인터

--- baekjoon_UTF8_2/src/baekjoon/용액.java
+++ baekjoon_UTF8_2/src/baekjoon/용액.java
@@ -3,7 +3,6 @@ package baekjoon;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.Arrays;
 import java.util.HashMap;
 import java.util.Map;
 import java.util.StringTokenizer;
@@ -40,7 +39,7 @@ public class 용액 {
                     right--;
             }
         }
-        System.out.println(Arrays.toString(map.get(key)));
+        System.out.println(map.get(key)[0]+" "+map.get(key)[1]);
     }
 }
 
