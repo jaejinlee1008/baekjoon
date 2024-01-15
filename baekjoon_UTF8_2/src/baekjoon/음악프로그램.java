1959fdd101df786b4270f51506211bd9b8d89e8a
jaejin <ghkfuddks@naver.com>
2024-01-15 22:15:46 +0900

    24/01/15_그래프

--- baekjoon_UTF8_2/src/baekjoon/음악프로그램.java
+++ baekjoon_UTF8_2/src/baekjoon/음악프로그램.java
@@ -1,7 +1,13 @@
 package baekjoon;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
 public class 음악프로그램 {
-	public static void main(String[] args) {
-		
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st = new StringTokenizer(br.readLine());
 	}
 }
