4c823d858eacd0b0fbb620937cc0d54442f3fa4d
jaejin <ghkfuddks@naver.com>
2025-04-08 23:54:14 +0900

    25/04/08_그리디,문자열 다른방식 풀이

--- baekjoon_UTF8_2/src/baekjoon/뒤집기.java
+++ baekjoon_UTF8_2/src/baekjoon/뒤집기.java
@@ -8,36 +8,15 @@ public class 뒤집기 {
 	public static void main(String[] args) throws Exception {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		String S = br.readLine();
-		int count0=0;
-		int count1=0;
+		int answer=1;
 		char curr = S.charAt(0);
-		if(curr=='1') {
-			count1++;
-		}else {
-			count0++;
-		}
 		for(int i=1;i<S.length();i++) {
 			if(curr!=S.charAt(i)) {
 				curr=S.charAt(i);
-				if(curr=='1') {
-					count1++;
-				}else {
-					count0++;
-				}
-				
+				answer++;
 			}
 		}
-		if(count1==0||count0==0) {
-			System.out.println(0);
-		}else if(count1==count0){
-			System.out.println(count0);
-		}else{
-			if(count1>count0) {
-				System.out.println(count0);
-			}else {
-				System.out.println(count1);
-			}
-		}
+		System.out.println(answer/2);
 		
 	}
 
