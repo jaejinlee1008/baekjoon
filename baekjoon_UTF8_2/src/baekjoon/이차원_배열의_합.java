9bcbf830fa2d665f1ff589072d80c36413f976d4
jaejin <ghkfuddks@naver.com>
2025-05-04 23:57:42 +0900

    25/05/04_풀이중

---
+++ baekjoon_UTF8_2/src/baekjoon/이차원_배열의_합.java
@@ -0,0 +1,35 @@
+package baekjoon;
+
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.util.StringTokenizer;
+
+public class 이차원_배열의_합 {
+	public static void main(String[] args) throws Exception{
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+		StringTokenizer st = new StringTokenizer(br.readLine());
+		int N = Integer.parseInt(st.nextToken());
+		int M = Integer.parseInt(st.nextToken());
+		int[] sum = new int[(N*M)+1];
+		for(int i=1;i<=N;i++) {
+			st = new StringTokenizer(br.readLine());
+			for(int j=1;j<=M;j++) {
+				sum[(i-1)*M+j]=sum[(i-1)*M+j-1]+Integer.parseInt(st.nextToken());
+			}
+		}
+		
+		int K = Integer.parseInt(br.readLine());
+		for(int k=0;k<K;k++) {
+			st = new StringTokenizer(br.readLine());
+			int i = Integer.parseInt(st.nextToken());
+			int j = Integer.parseInt(st.nextToken());
+			int x = Integer.parseInt(st.nextToken());
+			int y = Integer.parseInt(st.nextToken());
+			bw.write(sum[x*y]-sum[(i*j)-1]+"\n");
+		}
+		bw.flush();
+	}
+}
